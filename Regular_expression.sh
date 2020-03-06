f3f2a646ba78ef8a7e4917a8a160b413ea13877c
Kartikey45 <singhkartikey45@gmail.com>
2020-03-06 09:51:51 +0530

    ADDED : Enter valid first name

--- Regular_expression.sh
+++ Regular_expression.sh
@@ -1,3 +1,25 @@
 #!/bin/bash -x
 
-echo "Welcome to Regular Expression"
+echo "********** Welcome to Regular Expression **********"
+
+#CHECKING VALID OR INVALID CONDITIONS
+function conditionCheck()
+{
+	if [[ $value =~ $regexPattern ]]
+	then
+		echo "valid "
+	else
+		echo "invalid"
+	fi
+}
+
+#FUNCTION OF FIRST NAME
+function firstName()
+{
+	read -p "First name :" value
+	regexPattern="^[A-Z][a-zA-Z]{2,}$"
+	conditionCheck
+}
+
+#FUNCTIONS CALL
+firstName
