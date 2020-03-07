52b381abb49bde7b678ecd93f3717bec05d74c63
Kartikey45 <singhkartikey45@gmail.com>
2020-03-07 18:43:39 +0530

    Add project files.

---
+++ Employee_Wage_Computation_Using_C_sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employee_Wage_Computation_Using_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation !!!!!");
+            Console.ReadKey();
+        }
+    }
+}
