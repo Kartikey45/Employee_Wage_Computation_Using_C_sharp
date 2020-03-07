6b45125b1bc29aee4242e98b7dbad622e170cc7e
Kartikey45 <singhkartikey45@gmail.com>
2020-03-07 19:09:59 +0530

    ADDED : Employee is present or absent

--- Employee_Wage_Computation_Using_C_sharp/Program.cs
+++ Employee_Wage_Computation_Using_C_sharp/Program.cs
@@ -4,10 +4,28 @@ namespace Employee_Wage_Computation_Using_C_sharp
 {
     class Program
     {
-        static void Main(string[] args)
+        // MAIN METHOD
+        public static void Main(string[] args)
         {
+            // CREATE OBJECTS
+            Random random = new Random();
+
+            // INITIALIZE CONSTANTS AND VARIABLES
+            int IS_PRESENT = 1;
+            int num = random.Next(0, 2);
             Console.WriteLine("Welcome to Employee Wage Computation");
-            Console.ReadKey();
+            Console.Write("Attendance of Employee :\t");
+
+            //CONDITION CHECK OF EMPLOYEE IS PRESENT OR NOT
+            if (num == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present .");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent .");
+            }
+            Console.Read();
         }
     }
 }
