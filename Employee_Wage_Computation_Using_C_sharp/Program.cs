4afee9ba8d26d62aaa47ffe1416d57acf935708e
Kartikey45 <singhkartikey45@gmail.com>
2020-03-09 09:36:25 +0530

    ADDED : Calculte wages till total working hour or days is reached for a month

--- Employee_Wage_Computation_Using_C_sharp/Program.cs
+++ Employee_Wage_Computation_Using_C_sharp/Program.cs
@@ -10,41 +10,42 @@ namespace Employee_Wage_Computation_Using_C_sharp
             //CREATE OBJECTS
             Random random = new Random();
 
-            // INITIALIZE CONSTANTS AND VARIABLES
+            // INITIALIZE CONSTANTS AND VARIABLES                       
             int WAGE_PER_HOUR = 20;
             int hoursPerDay;
-            int dailyEmployeeWage;
             int salary = 0;
             int daysPerMonth = 20;
+            int days = 0;
+            int workingHours = 0;
+            int workingHoursLimit = 100;
 
             Console.WriteLine("Welcome to Employee Wage Computation");
 
             //CALCULATE WAGES FOR A MONTH
-            for (int day = 0; day < daysPerMonth; day++)
+            while (days < daysPerMonth && workingHours < workingHoursLimit)
             {
+                days++;
+
                 //CONDITION CHECK OF EMPLOYEE IS FULL-TIME , PART-TIME OR ABSENT
                 switch (random.Next(0, 3))
                 {
                     case 1:
-                        //Console.WriteLine("Employee is a Part time .");
                         hoursPerDay = 4;
                         break;
                     case 2:
-                        //Console.WriteLine("Employee is a Full time .");
                         hoursPerDay = 8;
                         break;
                     default:
-                        //Console.WriteLine("Employee is Absent .");
                         hoursPerDay = 0;
                         break;
                 }
 
-                //CALCULATE DAILY EMPLOYEE WAGE
-                dailyEmployeeWage = WAGE_PER_HOUR * hoursPerDay;
-
-                //CALCULATE THE SUM OF DAILY EMPLOYEE WAGES 
-                salary = salary + dailyEmployeeWage;
+                //CALCULATE TOTAL WORKING HOURS OF EMPLOYEE IN A MONTH
+                workingHours = workingHours + hoursPerDay;
             }
+
+            //CALCULATE EMPLOYEE WAGE FOR A MONTH
+            salary = WAGE_PER_HOUR * workingHours;
 
             //DISPLAY EMPLOYEE WAGE FOR A MONTH
             Console.WriteLine("Employee wage for a month :");
