7f8c2eaf3ab787243e88946cc447b61d6162c006
Kartikey45 <singhkartikey45@gmail.com>
2020-03-09 09:41:47 +0530

    ADDED : Write a function to get hours

--- Employee_Wage_Computation_Using_C_sharp/Program.cs
+++ Employee_Wage_Computation_Using_C_sharp/Program.cs
@@ -4,44 +4,59 @@ namespace Employee_Wage_Computation_Using_C_sharp
 {
     class Program
     {
+        //HOURS PER DAY OF AN EMPLOYEE
+        public int workingHoursPerDay(int hoursPerDay)
+        {
+            //CREATE OBJECT OF RANDOM CLASS 
+            Random random = new Random();
+
+            //CONDITION CHECK FOR FULL-TIME , PART-TIME OR ABSENT
+            switch (random.Next(0, 3))
+            {
+                case 1:
+                    hoursPerDay = 4;
+                    return hoursPerDay;
+                    break;
+                case 2:
+                    hoursPerDay = 8;
+                    return hoursPerDay;
+                    break;
+                default:
+                    hoursPerDay = 0;
+                    return hoursPerDay;
+                    break;
+            }
+            //return 0
+        }
+
         // MAIN METHOD
         public static void Main(string[] args)
         {
-            //CREATE OBJECTS
-            Random random = new Random();
-
             // INITIALIZE CONSTANTS AND VARIABLES                       
             int WAGE_PER_HOUR = 20;
-            int hoursPerDay;
             int salary = 0;
             int daysPerMonth = 20;
             int days = 0;
             int workingHours = 0;
             int workingHoursLimit = 100;
+            int hrsPerDay = 0;
+            int perDayHours;
 
             Console.WriteLine("Welcome to Employee Wage Computation");
 
-            //CALCULATE WAGES FOR A MONTH
+            //OBJECT CREATED
+            Program obj = new Program();
+
+            //CALCULATE TOTAL HOURS WORKED BY AN EMPLOYEE IN A MONTH OF TWENTY DAYS
             while (days < daysPerMonth && workingHours < workingHoursLimit)
             {
                 days++;
 
-                //CONDITION CHECK OF EMPLOYEE IS FULL-TIME , PART-TIME OR ABSENT
-                switch (random.Next(0, 3))
-                {
-                    case 1:
-                        hoursPerDay = 4;
-                        break;
-                    case 2:
-                        hoursPerDay = 8;
-                        break;
-                    default:
-                        hoursPerDay = 0;
-                        break;
-                }
+                //DECLARE VARIABLE
+                perDayHours = obj.workingHoursPerDay(hrsPerDay);
 
                 //CALCULATE TOTAL WORKING HOURS OF EMPLOYEE IN A MONTH
-                workingHours = workingHours + hoursPerDay;
+                workingHours = workingHours + perDayHours;
             }
 
             //CALCULATE EMPLOYEE WAGE FOR A MONTH
