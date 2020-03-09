c8dfae2e04fdd944b08346085733773cdf51fbce
Kartikey45 <singhkartikey45@gmail.com>
2020-03-09 09:07:39 +0530

    ADDED : Calculate daily employee wage

--- Employee_Wage_Computation_Using_C_sharp/Program.cs
+++ Employee_Wage_Computation_Using_C_sharp/Program.cs
@@ -12,7 +12,11 @@ namespace Employee_Wage_Computation_Using_C_sharp
 
             // INITIALIZE CONSTANTS AND VARIABLES
             int IS_PRESENT = 1;
+            int WAGE_PER_HOUR = 20;
+            int hoursPerDay;
             int num = random.Next(0, 2);
+            int dailyEmployeeWage;
+
             Console.WriteLine("Welcome to Employee Wage Computation");
             Console.Write("Attendance of Employee :\t");
 
@@ -20,11 +24,18 @@ namespace Employee_Wage_Computation_Using_C_sharp
             if (num == IS_PRESENT)
             {
                 Console.WriteLine("Employee is Present .");
+                hoursPerDay = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent .");
+                hoursPerDay = 0;
             }
+
+            //CALCULATE DAILY EMPLOYEE WAGE
+            Console.WriteLine("Daily Employee Wage :");
+            dailyEmployeeWage = WAGE_PER_HOUR * hoursPerDay;
+            Console.WriteLine(dailyEmployeeWage + " rs. per day");
             Console.Read();
         }
     }
